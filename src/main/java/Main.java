91b87a4741ed0b9ecd4f89b771f325c13672716b
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 07:08:14 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+       /**
+          The main method creates an instance of the
+          MyCatImage class which causes it to display
+          its window.
+       */
+       public static void main(String[] args)
+       {
+          new MyCatImage();
+       }
+}
